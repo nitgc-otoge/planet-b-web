9f4239581cb95ae9587cb3ab5c8a5832ad7acf44
snowsphere <info@snowsphere.net>
2024-03-05 03:57:45 +0900

    feat: フォントの追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,11 @@ export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Fira Sans', 'BIZ UDGothic', 'sans-serif']
+      }
+    }
+  }
 } satisfies Config;
