9b6f848927f1bb70a62143ef2011bb2eda23a209
snowsphere <info@snowsphere.net>
2024-03-09 01:34:37 +0900

    change: 404表記の変更

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -6,20 +6,20 @@ export default function Error404() {
       <Head>
         <title>404 - Page not found</title>
       </Head>
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
+      <div class="px-4 py-8 mx-auto">
         <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
           <img
             class="my-6"
             src="/logo.svg"
             width="128"
             height="128"
-            alt="the Fresh logo: a sliced lemon dripping with juice"
+            alt="PLANET-B logo"
           />
-          <h1 class="text-4xl font-bold">404 - Page not found</h1>
-          <p class="my-4">
-            The page you were looking for doesn't exist.
+          <h1 class="text-4xl font-bold">404</h1>
+          <p class="my-4 italic">
+            soko ni nakereba nai desune.
           </p>
-          <a href="/" class="underline">Go back home</a>
+          <a href="/" class="underline">Back</a>
         </div>
       </div>
     </>
